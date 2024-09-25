ce8dc16e2bc53a1f564ea21b78bb24a94d26ff88
Yourfirstname Yourlastname <your-email@example.com>
2024-09-26 09:45:10 +1000

    Added navigation bar

---
+++ navigation.php
@@ -0,0 +1,15 @@
+<nav class="nav_bar">
+  <ul class="menuList">
+    <li class="menuListLink">
+      <a class="menuClickeableLink homeLink" href="index.php">Home</a>
+    </li>
+    <li class="menuListLink">
+      <a class="menuClickeableLink" href="add.php">Register</a>
+    </li>
+  </ul>
+  <form role="search" class="search-container">
+    <input class="search-bar" type="search" aria-label="search results box" placeholder="search brands" id="search"
+      value="">
+    <input class="search-btn" type="button" value="search" aria-label="submision button">
+  </form>
+</nav>
